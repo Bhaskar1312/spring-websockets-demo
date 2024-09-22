6bb1b6b8315de253a49c9b537f2edf8d633a5062
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-09-22 23:38:33 +0530

    Initial project

---
+++ src/main/java/com/example/springwebsocketsdemo/SpringWebsocketsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebsocketsdemo/SpringWebsocketsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
